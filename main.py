ca1e4232a53754f25e1e7d1f610764cd4c00bdf6
William Fisher <ashton0312@gmail.com>
2018-12-05 16:15:10 -0500

    Added dinner

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from bs4 import BeautifulSoup;
 import urllib2;
 import json;
-import io;
 
-url = "https://www.elondining.com/locations/clohan-hall/?date=2018-12-05"
+lunchUrl = "https://www.elondining.com/locations/clohan-hall/?date=2018-12-05"
+dinnerUrl = ""
 
-content = urllib2.urlopen(url).read()
+content = urllib2.urlopen(lunchUrl).read()
 
 soup = BeautifulSoup(content)
 
-content = soup.find(id="menu-station-data-a1d0c6e83f027327d8461063f4ac58a6-d395771085aab05244a4fb8fd91bf4ee")
+lunch = soup.find(id="menu-station-data-a1d0c6e83f027327d8461063f4ac58a6-d395771085aab05244a4fb8fd91bf4ee")
 
-list = content.find_all('a')
+list = lunch.find_all('a')
 
 items = {}
 
@@ -20,5 +20,18 @@ for idx, child in enumerate(list):
 
 print (json.dumps(items))
 
-with open('data.json', 'w') as outfile:
+with open('lunch.json', 'w') as outfile:
+    json.dump(items, outfile)
+
+items.clear()
+
+dinner = soup.find(id="menu-station-data-a1d0c6e83f027327d8461063f4ac58a6-e3796ae838835da0b6f6ea37bcf8bcb7")
+list = dinner.find_all('a')
+
+for idx, child in enumerate(list):
+    items[idx] = (child.string+".")
+
+print(json.dumps(items))
+
+with open('dinner.json', 'w') as outfile:
     json.dump(items, outfile)
